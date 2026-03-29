9d1fb018f114b788feb8a4f2e7b998d428e02fc2
rromerodev3 <rromerodev@gmail.com>
2026-03-28 19:44:34 -0600

    add getCharactersUseCase

---
+++ app/src/main/java/com/roy/rromeroarkanochallenge/domain/usecase/GetCharactersUseCase.kt
@@ -0,0 +1,17 @@
+package com.roy.rromeroarkanochallenge.domain.usecase
+
+import com.roy.rromeroarkanochallenge.domain.model.Character
+import com.roy.rromeroarkanochallenge.domain.repository.CharacterRepository
+import javax.inject.Inject
+
+class GetCharactersUseCase @Inject constructor(
+    private val repository: CharacterRepository
+) {
+    suspend operator fun invoke(): Result<List<Character>> {
+        return try {
+            Result.success(repository.getCharacters())
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+}
